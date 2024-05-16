62ca68d396b8632973106891374ad9d33601a28e
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-16 10:11:46 +0200

    endpoints getAll, getById,getTaskByTitle

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -10,6 +10,34 @@ const TaskController = {
       console.error(error)
       res.status(500).send("Ha habido un error al crear la tarea")
     }
+  },
+  async getAll(req,res) {
+    try {
+      const tasks = await Task.find()
+      res.send({msg: "Todos las tareas: " , tasks})
+    } catch (error) {
+      console.error(error)
+      res.status(500).send("Ha habido un error")
+    }
+  },
+  async getById(req,res) {
+    try {
+      const task = await Task.findById(req.params._id)
+      res.send({msg: "Tarea encontrada", task})
+    } catch (error) {
+      console.error(error)
+      res.status(500).send("Ha habido un error encontrando la tarea")
+    }
+  },
+  async getTaskByTitle(req,res) {
+    try {
+      const title = new RegExp(req.params.title, "i")
+      const task = await Task.find({ title })
+      res.send({msg:"Tarea encontrada", task})
+    } catch (error) {
+      console.error(error)
+      res.status(500).send("Ha habido un error encontrando la tarea por título")
+    }
   }
 }
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -5,6 +5,9 @@ const TaskController = require("../controllers/TaskController")
 
 
 router.post("/", TaskController.create)
+router.get("/", TaskController.getAll)
+router.get("/id/:_id", TaskController.getById)
+router.get("/title/:title", TaskController.getTaskByTitle)
 
 
 module.exports = router
